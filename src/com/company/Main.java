48cce5814dfe0fae23f9d8725ee68024cb7fbcc0
ge35pum <>
2021-02-09 14:54:03 +0100

    revert change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Code that works fine");
         System.out.println("Change that works well");
-        oopsie daisy code that makes no sense
     }
 }
