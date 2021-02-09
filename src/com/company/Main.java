f5d8fe26c64dc80f5aaf745a246c6d3c3cac44a4
ge35pum <>
2021-02-09 15:17:59 +0100

    reverted the second stupid mistake

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        foobar
         System.out.println("Code that works fine");
         System.out.println("Change that works well");
         System.out.println("Change that makes sense again");
