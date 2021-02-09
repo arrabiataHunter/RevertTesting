dbac903a9cd17e956ab63d913b9dcf56954e04d1
ge35pum <>
2021-02-09 15:16:44 +0100

    i definitely should get fired

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        foobar
         System.out.println("Code that works fine");
         System.out.println("Change that works well");
         System.out.println("Change that makes sense again");
